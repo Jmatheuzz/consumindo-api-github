c354d4da956fd49e883a4052fc6aa939c4f5b953
João Matheus <joaomatheusantunes@gmail.com>
2022-03-04 10:29:30 -0300

    configurando deploy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
-    <Router history={history}>
+    <Router basename={process.env.PUBLIC_URL} history={history}>
       <Routes/>
       <GlobalStyles/>
       <ToastContainer autoClose={5000} className="toast-container"/>
