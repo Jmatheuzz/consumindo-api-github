47a12b96ff11c096c57c6d44b48ad1fdd84c35c5
João Matheus <joaomatheusantunes@gmail.com>
2022-03-08 15:29:54 -0300

    Updates

--- src/messages/Error/index.js
+++
@@ -1,6 +0,0 @@
-import { toast } from "react-toastify";
-export default function error(){
-    toast.error("Error", {
-        toastId: "customId1"
-    });
-}
--- src/messages/Success/index.js
+++
@@ -1,6 +0,0 @@
-import { toast } from "react-toastify";
-export default function success(){
-    toast.success("Success", {
-        toastId: "customId"
-    });
-}
